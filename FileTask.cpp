a74df6fe6e75342a62233250d082e7647c1aef77
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-11-28 15:25:04 +0100

    algorthim for displaying file names

--- FileTask.cpp
+++ FileTask.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+ 
+void showMenu() {
+	std::vector < std::string > info= { "For creating a file click 1\n", "For writing to an existing file choose 2\n", "to quit write 3\n" };
+	
+	for (int i = 0; i < info.size(); i++) {
+		std::cout << info[i];
 
-int main (){
-
-    
+	}
 }
+std::vector<std::string> premadefileNames = { "example.txt", "example2.txt"};
 
+void displayExistingFiles() {
+	std::cout << "Existing files\n";
+	for (int i = 0; i < premadefileNames.size(); i++) {
+		std::cout << premadefileNames[i] << "\n";
+	}
+}
